bcdeb8a3288d8ef3e69e39bbd24f2616d82fa6e0
kalyandhar84 <49374382+kalyandhar84@users.noreply.github.com>
2022-04-26 01:01:18 -0400

    update1

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def homepage():
 @app.route('/cpp/', methods=['GET', 'POST'])
 def cpp():
     t=""
+    #my_path = os.path.dirname(__file__)
     my_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     req_type = request.method
     if req_type == 'GET':
@@ -192,6 +193,7 @@ def get_summary(df_1yr, symbol):
     plt.ylabel('Stock Price', fontsize=13)
     plt.xlabel('Date', fontsize=13)
     plt.title("Stock Price Movement - "+symbol, fontsize=20)
+    #my_path = os.path.dirname(__file__)
     my_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) # Figures out the absolute path for you in case your working directory moves around.
 
     plt.savefig(my_path + '\\static\\images\\stock_summary.jpg')
@@ -225,6 +227,7 @@ def ml_modelprocess(df_model, df_future, symbol):
     plt.ylabel('Stock Price', fontsize=13)
     plt.xlabel('Date', fontsize=13)
     plt.title(symbol + " Stock Price Movement with Forecast", fontsize=20)
+    #my_path = os.path.dirname(__file__)
     my_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     plt.savefig(my_path + '\\static\\images\\stock_forecast.jpg')
 
